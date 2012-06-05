867eff03e8b8e51e748da51782867ef92cb15234
Stefan Otte <stefan.otte@gmail.com>
2012-06-05 06:29:42 +0200

    Extend PrecisePositionsApp.
    make debugging easier.

--- test/de/fhpotsdam/unfolding/PrecisePositionsApp.java
+++ test/de/fhpotsdam/unfolding/PrecisePositionsApp.java
@@ -6,17 +6,24 @@ import de.fhpotsdam.unfolding.Map;
 import de.fhpotsdam.unfolding.geo.Location;
 import de.fhpotsdam.unfolding.utils.MapUtils;
 
+/**
+ * Three points which are very close to each other (locations) get displayed.
+ * 
+ * They should be visible as seperate points
+ */
 public class PrecisePositionsApp extends PApplet {
 
 	Map map;
 
 	public static final Location SINGAPORE_LOCATION = new Location(1.283f, 103.833f);
+
+	// really close points
 	Location location1 = new Location(1.283000f, 103.833000f);
 	Location location2 = new Location(1.283001f, 103.833001f);
 	Location location3 = new Location(1.283018f, 103.833015f);
 
 	public void setup() {
-		size(800, 600, GLConstants.GLGRAPHICS);
+		size(800, 600);
 
 		map = new Map(this);
 		map.zoomAndPanTo(SINGAPORE_LOCATION, 14);
@@ -37,27 +44,40 @@ public class PrecisePositionsApp extends PApplet {
 	public void draw() {
 		background(0);
 		map.draw();
-
+		
+		// locations to pos
 		float[] pos1 = map.getScreenPositionFromLocation(location1);
 		float[] pos2 = map.getScreenPositionFromLocation(location2);
 		float[] pos3 = map.getScreenPositionFromLocation(location3);
-		// println("1 " + pos1[0] + "," + pos1[1]);
-		// println("2 " + pos2[0] + "," + pos2[1]);
-		// println("3 " + pos3[0] + "," + pos3[1]);
+		println("1 " + pos1[0] + "," + pos1[1]);
+		println("2 " + pos2[0] + "," + pos2[1]);
+		println("3 " + pos3[0] + "," + pos3[1]);
 
 		Location locA = map.getLocationFromScreenPosition(mouseX, mouseY);
 		Location locB = map.getLocationFromScreenPosition(mouseX + 5, mouseY + 5);
 		ellipse(mouseX, mouseY, 10, 10);
 		ellipse(mouseX + 5, mouseY + 5, 10, 10);
-		println("locA " + locA);
-		println("locB " + locB);
+		//println("locA " + locA.x + " " + locA.y);
+		//println("locB " + locB.x + " " + locB.y);
 
+		// draw the three positions
 		fill(255, 0, 0);
 		ellipse(pos1[0], pos1[1], 6, 6);
+		text("pos1", pos1[0], pos1[1]);
+
 		fill(0, 255, 0);
 		ellipse(pos2[0], pos2[1], 6, 6);
+		text("pos2", pos2[0], pos2[1]);
+		
 		fill(0, 0, 255);
 		ellipse(pos3[0], pos3[1], 6, 6);
+		text("pos3", pos3[0], pos3[1]);
+
 	}
 
+	public static void main(String[] args) {
+		// Here we start the actual Unfolding part
+		PApplet.main(new String[] { "de.fhpotsdam.unfolding.PrecisePositionsApp" });
+
+	}
 }
